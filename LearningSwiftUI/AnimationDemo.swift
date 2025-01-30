d68dd5990334169e976056a40336d120a7ea0bcb
Maulik shah <maulik_shah@hotmail.co.in>
2025-01-30 19:31:49 +0530

    Animation demo learning

---
+++ LearningSwiftUI/AnimationDemo.swift
@@ -0,0 +1,38 @@
+//
+//  Animation.swift
+//  LearningSwiftUI
+//
+//  Created by Maulik Shah on 1/30/25.
+//
+
+import SwiftUI
+
+struct AnimationDemo: View {
+    @State var isAnimated = false
+    
+    var body: some View {
+        VStack {
+            Button("Animate View") {
+                     isAnimated.toggle()
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? .red : .green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 :300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(.default
+                           .repeatForever()
+                           , value: isAnimated)
+                
+                 
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationDemo()
+}
--- LearningSwiftUI/TurneryOperator.swift
+++ LearningSwiftUI/TurneryOperator.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct TurneryOperator: View {
+    @State var isStartingState = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+           
+            Button("Button : \(isStartingState.description)"){
+                isStartingState.toggle()
+            }
+            
+            Text(isStartingState ? "Starting State!!!" : "Ending State..." )
+            
+            RoundedRectangle(cornerRadius: isStartingState ? 25 : 0)
+                .fill(isStartingState ? .red : .blue)
+                .frame(
+                    width: isStartingState ? 200 : 50,
+                    height: isStartingState ? 400 : 50)
+            
+            Spacer()
+        }
+       
     }
 }
 
