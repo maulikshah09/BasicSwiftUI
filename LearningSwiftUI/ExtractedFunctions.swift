b41cf7e267dca6e203acdc7fd2729185e66cb071
Maulik shah <maulik_shah@hotmail.co.in>
2025-01-30 17:28:09 +0530

    ExtractFunctions

---
+++ LearningSwiftUI/ExtractedFunctions.swift
@@ -0,0 +1,52 @@
+//
+//  MakeCommonViews.swift
+//  LearningSwiftUI
+//
+//  Created by Maulik Shah on 1/30/25.
+//
+
+import SwiftUI
+
+struct ExtractedFunctions: View {
+    
+    @State var backgroundColor: Color = .pink
+    
+    var body: some View {
+        ZStack{
+            // backgroud
+            backgroundColor.ignoresSafeArea()
+          
+            //content
+            contentLayer
+        }
+    }
+    
+    // so many UI then make something like this.
+    var contentLayer : some View{
+        VStack{
+            Text("Title")
+                .font(.largeTitle)
+            
+            Button {
+                buttonPress()
+            } label: {
+                Text("Press Me")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                   
+            }
+
+        }
+    }
+    
+    func buttonPress () { // if so many logic then make func and work here and call.
+        backgroundColor = .yellow
+    }
+}
+
+#Preview {
+    ExtractedFunctions()
+}
