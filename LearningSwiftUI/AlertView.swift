3f729d1401d00626bffa35acf1f9b3773dc44449
Maulik shah <maulik_shah@hotmail.co.in>
2025-01-31 18:14:37 +0530

    ListView leraned.

---
+++ LearningSwiftUI/AlertView.swift
@@ -0,0 +1,18 @@
+//
+//  AlertView.swift
+//  LearningSwiftUI
+//
+//  Created by Maulik Shah on 1/31/25.
+//
+
+import SwiftUI
+
+struct AlertViewDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertViewDemo()
+}
--- LearningSwiftUI/ListDemo.swift
+++ LearningSwiftUI/ListDemo.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct ListDemo: View {
     @State var fruits: [String] = ["Apples", "Bananas", "Oranges","peach"]
+    
+    @State var veggies: [String] = ["Carrots", "Broccoli", "Spinach"]
    
     var body: some View {
         
-         
+        NavigationStack{
             List {
                 
-                Section(header: Text("Fruits")) {
+                Section(header:
+                    HStack{
+                        Text("Fruits")
+                        Image(systemName: "flame.fill")
+                    }
+                    .font(.headline)
+                    .foregroundColor(.orange)) {
                     ForEach(fruits,id: \.self){ fruit in
                         Text(fruit)
+                            .font(.caption)
+                            .foregroundStyle(.white)
+                            .padding(.vertical)
                     }
-                    
+                  
                     // first way
     //                .onDelete { indexset in
     //                    delete(indexSet: indexset)
@@ -27,21 +38,57 @@ struct ListDemo: View {
                     
                     //second way
                     .onDelete(perform: delete)
+                    
+                    
+                    // firstWay
+//                    .onMove { indices, newoffset in
+//                      
+//                        fruits.move(fromOffsets: indices, toOffset: newoffset)
+//                    }
+//
+                    //Second way
+                    .onMove(perform: move)
                 }
+                .listRowBackground(.pink)
                 
                 Section(header: Text("Vegitables")) {
-                    ForEach(fruits,id: \.self){ fruit in
-                        Text(fruit)
+                    ForEach(veggies,id: \.self){ veggies in
+                        Text(veggies)
                     }
                 }
                 
             }
-            .listStyle(.plain)
+            .tint(.yellow)
+            //.listStyle(PlainListStyle())
+           // .listStyle(DefaultListStyle())
+           // .listStyle(GroupedListStyle())
+          //.listStyle(InsetGroupedListStyle())
+            //.listStyle(SidebarListStyle())
+
             
             .navigationTitle("Grocery List")
-            
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    addButton
+                }
+            }
         }
+        .tint(.red)
+    }
+    
+    var addButton : some View{
+        Button("Add"){
+            fruits.append("coconut")
+        }
+    }
    
+    func move(indices: IndexSet, newoffSet: Int){
+        fruits.move(fromOffsets:indices, toOffset: newoffSet)
+    }
     
     func delete(indexSet : IndexSet) {
         fruits.remove(atOffsets: indexSet)
