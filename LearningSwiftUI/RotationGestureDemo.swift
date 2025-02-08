2361c014c4baca4f780fd940637b000480c050df
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-08 10:22:15 +0530

    Rotationgesture Done.

--- LearningSwiftUI/RotationGestureDemo.swift
+++ LearningSwiftUI/RotationGestureDemo.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct RotationGestureDemo: View {
+    @State var angle : Angle = Angle(degrees: 0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundStyle(.white)
+            .padding(50)
+            .background(.blue)
+            .cornerRadius(10)
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged{ value in
+                        angle = value
+                    }.onEnded{value  in
+                        withAnimation {
+                            angle = Angle(degrees: 0)
+                        }
+                    }
+            )
     }
 }
 
