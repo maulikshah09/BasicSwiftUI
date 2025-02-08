084f2a6b374824f912458f02b65d322108b1717c
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-09 00:06:39 +0530

    ScrollviewReader

---
+++ LearningSwiftUI/GeometryRaderDemo.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryRaderDemo.swift
+//  LearningSwiftUI
+//
+//  Created by Maulik Shah on 2/9/25.
+//
+
+import SwiftUI
+
+struct GeometryRaderDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryRaderDemo()
+}
---
+++ LearningSwiftUI/ScrollViewReaderDemo.swift
@@ -0,0 +1,63 @@
+//
+//  SwiftUIViewReader.swift
+//  LearningSwiftUI
+//
+//  Created by Maulik Shah on 2/8/25.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderDemo: View {
+  
+    @State var scrollToIndx : Int = 0
+    @State var textFieldText : String = ""
+    
+    var body: some View {
+        VStack(spacing: 12.0) {
+            TextField("Enter a # here..",text: $textFieldText)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(Color.white)
+                .cornerRadius(10)
+                .border(.gray)
+               
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW"){
+                withAnimation(.spring) {
+                    if let index = Int(textFieldText){
+                        scrollToIndx = index
+                    }
+                }
+            }
+            
+            ScrollView{
+                ScrollViewReader { proxy in
+                    ForEach(0..<50){index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height : 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndx) { newValue in
+                        withAnimation(.spring){
+                            proxy.scrollTo(newValue,anchor: nil)
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SwiftUIViewReaderDemo()
+}
